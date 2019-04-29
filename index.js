f7dd0aeb05a7e5616fbcaf9ca2f582d0eca25af1
Umesh K <u.teach.and.i.learn@gmail.com>
2019-04-29 05:40:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
-  delete newObj.key
-
-}
-
-// function destructivelyDeleteFromObjectByKey(object, key) {
-//   delete object.key
-//   return object
+// function deleteFromObjectByKey(object, key) {
+//   var newObj = Object.assign({}, object)
+//   delete newObj.key
+//   return newObj
 // }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
